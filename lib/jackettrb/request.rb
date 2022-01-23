c154870bbe3e7f33e082faeb5469442e3b431bc4
Josh Burns <joshyburnss@gmail.com>
2022-01-23 15:05:48 +1100

    Request class that handles API communications

---
+++ lib/jackettrb/request.rb
@@ -0,0 +1,34 @@
+require "net/http"
+require "open-uri"
+
+class Request
+  def initialize(key, host, strict?=false)
+    @key = key
+    @host = host
+    @strict = strict?
+  end
+
+  def firehose
+    url = parse_url(@host, nil)
+    parse_rss(url, @strict)
+  end
+
+  def query(q)
+    url = parse_url(@host, "&q=#{q}")
+    parse_rss(url, @strict)
+  end
+
+  private
+
+  def parse_url(host, params)
+    URI.parse("#{host}/api/v2.0/indexers/all/results/torznab/api?apikey=#{@key}#{params}")
+  end
+
+  def parse_rss(url, strict?)
+    feed = nil
+    URI.open(url) do |rss|
+      feed = RSS::Parser.parse(rss, strict?)
+    end
+    feed
+  end
+end
