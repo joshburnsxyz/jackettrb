1673304163094075126ea2757d24c18555341ef2
Josh Burns <joshyburnss@gmail.com>
2022-01-23 15:10:59 +1100

    some tests

--- test/jackettrb_test.rb
+++ test/jackettrb_test.rb
@@ -7,7 +7,16 @@ class JackettrbTest < Minitest::Test
     refute_nil ::Jackettrb::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_request_class_exists
+    refute_nil ::Jackettrb::Request.new("127.0.0.1:9117", "thisisanapikeyipromise", false)
+  end
+
+  def test_request_class_has_default_value_for_strict
+    refute_nil ::Jackettrb::Request.new("127.0.0.1:9117", "thisisanapikeyipromise")
+  end
+
+  def test_default_value_for_strict_is_false
+    r = ::Jackettrb::Request.new("127.0.0.1:9117", "thisisanapikeyipromise")
+    assert_equal false, r.strict
   end
 end
